e6785812b180d74a82fabb357b428d4b7c519e37
mysterious-diamond <aaronkenn10@gmail.com>
2026-08-12 13:24:15 +0700

    Made backend foundation spring boot

---
+++ backend/src/test/java/com/aarons/videochat/VideochatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarons.videochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideochatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
